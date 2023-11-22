87fba446ad5182d4a2edf737d751aa7b83ec4f61
Asbayazitog1 <as_bayazit@outlook.com>
2023-11-22 15:04:08 +0000

    task 5 completed

--- db/app.model.js
+++ db/app.model.js
@@ -19,3 +19,28 @@ const queryString =`SELECT * FROM articles WHERE article_id =$1;`
     return result.rows[0]
   })
 }
+
+exports.selectAllArticles = (query) =>{
+  const sort_by = query.sort_by
+  const order = query.order
+  const allowedSortBy =['author','title','article_id','topic','created_at','votes','article_img_url','comment_count']
+  if(sort_by && !allowedSortBy.includes(sort_by)){
+    return Promise.reject({ status: 400, msg: "Invalid sort option" })
+  }
+  let queryString =`SELECT *,(SELECT COUNT(article_id) FROM comments WHERE articles.article_id = comments.article_id) AS comment_count FROM articles`
+
+  const sortOrder =(order && order ==="asc")?'ASC':'DESC'
+
+  if(sort_by){
+    queryString +=` ORDER BY ${sort_by} ${sortOrder};`
+  }
+
+
+  return db.query(queryString).then(({rows})=>{
+    rows.forEach(row => {
+      row.comment_count =+row.comment_count
+    })
+    
+    return rows
+  })
+}
